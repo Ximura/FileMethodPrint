d90024f402859d5364f823a4ff05c61c1db03cd3
Ximura <orukin@gmail.com>
2017-08-22 23:49:21 +0300

    cross platform support
    support different platforms

--- Trace.h
+++ Trace.h
@@ -1,16 +1,19 @@
 #pragma once
 
+
+
 constexpr const char* str_end(const char *str) {
   return *str ? str_end(str + 1) : str;
 }
 
 constexpr bool str_slant(const char *str) {
-  return *str == '\\' ? true : (*str ? str_slant(str + 1) : false);
+  return *str == '/' || *str == '\\' ? true : (*str ? str_slant(str + 1) : false);
 }
 
 constexpr const char* r_slant(const char* str) {
-  return *str == '\\' ? (str + 1) : r_slant(str - 1);
+  return *str == '/' || *str == '\\' ? (str + 1) : r_slant(str - 1);
 }
+
 constexpr const char* file_name(const char* str) {
   return str_slant(str) ? r_slant(str_end(str)) : str;
 }
